7fc7a6cfa0c3f383b0c29633df126ab790505ecf
abc <ABCSSS2021@outlook.com>
2021-08-17 19:25:19 +0800

    撤回

---
+++ game_star.py
@@ -0,0 +1,16 @@
+class GameStars:
+    """跟踪游戏"""
+
+    def __init__(self, ai_settings):
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        self.game_active = False
+        # 最高得分
+        with open(" record/high_score.txt", "r") as high:
+            self.high_score = int(high.read())
+
+    def reset_stats(self):
+        # 初始化信息
+        self.ship_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
